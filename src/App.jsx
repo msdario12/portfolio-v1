036c190a7a18d051f56dd80cc0190774d8ae4eaa
msdario12 <mansilladario94@gmail.com>
2023-08-20 11:34:31 -0300

    added title in app main section

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 	return (
 		<main className='bg-slate-900 text-white min-h-screen flex flex-col justify-between bg-gradient-to-b from-violet-600/[.15] via-transparent'>
 			<CustomHero />
-			<h2 className='text-3xl leading-tight font-bold md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight bg-clip-text bg-gradient-to-r from-blue-600 to-violet-600 text-transparent'>
+			<h2 className='text-3xl leading-tight font-bold md:text-5xl md:leading-tight lg:text-5xl lg:leading-tight bg-clip-text bg-gradient-to-r from-white to-gray-600 text-transparent container mx-auto px-4'>
 				Proyectos
 			</h2>
 			<PortfolioSection />
