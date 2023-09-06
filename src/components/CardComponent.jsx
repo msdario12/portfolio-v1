57649c7dc8d37e133eb0a82035bc27c64db6f22f
msdario12 <mansilladario94@gmail.com>
2023-09-05 22:24:31 -0300

    added animations to portfolio section
    added framer motion animation to each card, and effect in group

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,23 +1,25 @@
-import { Github } from 'react-bootstrap-icons';
-import { VscLiveShare } from 'react-icons/vsc';
+import { Github } from 'react-bootstrap-icons'
+import { VscLiveShare } from 'react-icons/vsc'
+import { motion, useAnimate, useInView } from 'framer-motion'
+import { useEffect } from 'react'
 
 const Title = ({ title }) => {
 	return (
-		<div className='text-sm font-bold text-gray-800 rounded-lg bg-white p-4 md:text-xl dark:bg-gray-800 dark:text-gray-200'>
+		<div className="text-sm font-bold text-gray-800 rounded-lg bg-white p-4 md:text-xl dark:bg-gray-800 dark:text-gray-200">
 			{title}
 		</div>
-	);
-};
+	)
+}
 function Icons({ children }) {
 	return (
-		<div className='flex gap-3 justify-center items-center flex-wrap'>
+		<div className="flex gap-3 justify-center items-center flex-wrap">
 			{children}
 		</div>
-	);
+	)
 }
 
 function Description({ children }) {
-	return <p className=' text-gray-200 mb-5'>{children}</p>;
+	return <p className=" text-gray-200 mb-5">{children}</p>
 }
 const CardComponent = ({
 	urlImg,
@@ -28,7 +30,7 @@ const CardComponent = ({
 	deployLink,
 	repoLink,
 }) => {
-	const className = 'bottom-full top-0 top-full left-0 left-full';
+	const className = 'bottom-full top-0 top-full left-0 left-full'
 	const options = {
 		top: {
 			from: 'top-full',
@@ -46,56 +48,78 @@ const CardComponent = ({
 			from: 'left-0',
 			to: 'left-full',
 		},
-	};
+	}
+	const [scope, animate] = useAnimate()
+	const isInView = useInView(scope)
+
+	useEffect(() => {
+		if (isInView) {
+			animate(
+				scope.current,
+				{ opacity: 1 },
+				{ duration: 1.0 },
+				{ delay: 0.8 }
+			)
+		} else {
+			animate(scope.current, { opacity: 0 })
+		}
+	}, [isInView])
 	return (
-		<button
-			onClick={() => setIsModalOpen((prev) => !prev)}
-			className='group relative block rounded-xl overflow-hidden h-full'>
-			<div className='rounded-xl overflow-hidden h-full'>
+		<div
+			ref={scope}
+			// onClick={() => setIsModalOpen((prev) => !prev)}
+			className="group relative block rounded-xl overflow-hidden h-full"
+		>
+			<div className="rounded-xl overflow-hidden h-full">
 				<img
-					className='group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl h-full w-full object-cover object-center'
+					className="group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl h-full w-full object-cover object-center"
 					src={urlImg}
-					alt='Imagen de presentación de un proyecto de programación'
+					alt="Imagen de presentación de un proyecto de programación"
 				/>
 			</div>
-			<div className='absolute bottom-0 left-0 right-0 p-2 sm:p-4'>
+			<div className="absolute bottom-0 left-0 right-0 p-2 sm:p-4">
 				<Title title={title} />
 			</div>
 			<div
-				className={`group-hover:top-0 absolute top-full bg-gray-800/80 rounded-lg p-4 transition-all lg:m-4 m-4  ease-in-out delay-75 duration-200   backdrop-blur`}>
+				className={`group-hover:top-0 absolute top-full bg-gray-800/80 rounded-lg p-4 transition-all lg:m-4 m-4  ease-in-out delay-75 duration-200   backdrop-blur`}
+			>
 				{children}
 			</div>
 			<div
-				className={`group-hover:opacity-100 bottom-0 opacity-0 absolute rounded-lg transition-all ease-in-out delay-75 duration-200 `}>
+				className={`group-hover:opacity-100 bottom-0 opacity-0 absolute rounded-lg transition-all ease-in-out delay-75 duration-200 `}
+			>
 				<a
-					className='relative text-slate-500 z-30 flex justify-center item rounded-se-full aspect-square bg-gradient-to-tr from-slate-500/60 to-50% hover:text-white transition-all duration-200 delay-75 p-7 group/item font-bold'
+					className="relative text-slate-500 z-30 flex justify-center item rounded-se-full aspect-square bg-gradient-to-tr from-slate-500/60 to-50% hover:text-white transition-all duration-200 delay-75 p-7 group/item font-bold"
 					href={repoLink}
-					target='_blank'
+					target="_blank"
 					onClick={(e) => e.stopPropagation()}
-					rel='noreferrer'>
-					<span className='group-hover/item:animate-bounce'>
+					rel="noreferrer"
+				>
+					<span className="group-hover/item:animate-bounce">
 						<Github size={30} />
 					</span>
-					<div className='absolute bottom-0 left-0 rounded-full w-1 h-1 group-hover/item:scale-[55] bg-slate-500 shadow-lg transition-all duration-150 delay-75 -z-20'></div>
+					<div className="absolute bottom-0 left-0 rounded-full w-1 h-1 group-hover/item:scale-[55] bg-slate-500 shadow-lg transition-all duration-150 delay-75 -z-20"></div>
 				</a>
 			</div>
 			<div
-				className={`group-hover:opacity-100 bottom-0 right-0 opacity-0 absolute rounded-lg transition-all ease-in-out delay-75 duration-200 `}>
+				className={`group-hover:opacity-100 bottom-0 right-0 opacity-0 absolute rounded-lg transition-all ease-in-out delay-75 duration-200 `}
+			>
 				<a
-					className='relative text-slate-500 z-30 flex justify-center item rounded-et-full aspect-square bg-gradient-to-tl from-slate-500/60 to-50% hover:text-white transition-all duration-200 delay-75 p-7 group/item font-bold'
+					className="relative text-slate-500 z-30 flex justify-center item rounded-et-full aspect-square bg-gradient-to-tl from-slate-500/60 to-50% hover:text-white transition-all duration-200 delay-75 p-7 group/item font-bold"
 					href={deployLink}
-					target='_blank'
+					target="_blank"
 					onClick={(e) => e.stopPropagation()}
-					rel='noreferrer'>
-					<span className='group-hover/item:animate-bounce group-hover:animate-pulse'>
+					rel="noreferrer"
+				>
+					<span className="group-hover/item:animate-bounce group-hover:animate-pulse">
 						<VscLiveShare size={30} />
 					</span>
-					<div className='absolute bottom-0 right-0 rounded-full w-1 h-1 group-hover/item:scale-[55] bg-slate-500 shadow-lg transition-all duration-150 delay-75 -z-20'></div>
+					<div className="absolute bottom-0 right-0 rounded-full w-1 h-1 group-hover/item:scale-[55] bg-slate-500 shadow-lg transition-all duration-150 delay-75 -z-20"></div>
 				</a>
 			</div>
-		</button>
-	);
-};
-CardComponent.Icons = Icons;
-CardComponent.Description = Description;
-export default CardComponent;
+		</div>
+	)
+}
+CardComponent.Icons = Icons
+CardComponent.Description = Description
+export default CardComponent
